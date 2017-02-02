1df6f7dd1e3ae3fd568da77680e87424ebc97311
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-03 01:04:20 +0530

    fill text with stroke

--- canvas3.js
+++ canvas3.js
@@ -1,8 +1,8 @@
 var can = document.getElementById("Newcanvas");
 var Jctx = can.getContext("2d");
 Jctx.font = "40px Verdana";
-//Jctx.fillStyle = "Blue";
-//Jctx.fillText("GymKhana Election",10,50);
+Jctx.fillStyle = "Blue";
+Jctx.fillText("GymKhana Election",10,80);
 
 Jctx.strokeStyle = "Red";
-Jctx.strokeText("GymKhana Election",10,80);
+Jctx.strokeText("GymKhana Election",13,83);
