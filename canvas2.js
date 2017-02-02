f432654584f91cf6cb437585cbb3c24d6236cbd7
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-03 00:44:11 +0530

    mickey mouse

--- canvas2.js
+++ canvas2.js
@@ -17,3 +17,48 @@ Jctx.lineTo(300,200);
 Jctx.lineWidth = 10;
 Jctx.strokeStyle = "blue";
 Jctx.stroke();
+
+
+var can2 = document.getElementById("Newcanvas2");
+var Jctx2 = can2.getContext("2d");
+Jctx2.beginPath();
+Jctx2.arc(200,200,100,0,2*Math.PI);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
+
+Jctx2.beginPath();
+Jctx2.arc(160,160,24,0,2*Math.PI);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
+
+Jctx2.beginPath();
+Jctx2.arc(300,100,40,0,2*Math.PI);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
+
+Jctx2.beginPath();
+Jctx2.arc(100,100,40,0,2*Math.PI);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
+
+Jctx2.beginPath();
+Jctx2.arc(240,160,24,0,2*Math.PI);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
+
+
+Jctx2.moveTo(200,215);
+Jctx2.lineTo(200,190);
+Jctx2.stroke();
+
+
+Jctx2.beginPath();
+Jctx2.arc(200,230,24,0,3.3);
+Jctx2.strokeStyle = "green";
+Jctx2.lineWidth=4;
+Jctx2.stroke();
