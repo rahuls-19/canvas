dc53cdf289ed2db90f09acdee8a4267019dad745
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-03 00:58:53 +0530

    fill text method

---
+++ canvas3.js
@@ -0,0 +1,8 @@
+var can = document.getElementById("Newcanvas");
+var Jctx = can.getContext("2d");
+Jctx.font = "40px Verdana";
+//Jctx.fillStyle = "Blue";
+//Jctx.fillText("GymKhana Election",10,50);
+
+Jctx.strokeStyle = "Red";
+Jctx.strokeText("GymKhana Election",10,80);
