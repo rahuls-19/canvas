1cd2e395d9de510df3b24b28ca3279d2fdead81e
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-02 23:01:35 +0530

    design using lines

--- canvas2.js
+++ canvas2.js
@@ -1,6 +1,19 @@
 var can = document.getElementById("Newcanvas");
 var Jctx = can.getContext("2d");
-Jctx.moveTo(10,10);// x-axis y-axis
-Jctx.lineTo(200,400);
+Jctx.moveTo(200,0);// x-axis y-axis
+Jctx.lineTo(00,400);
+Jctx.lineWidth = 10;
+Jctx.strokeStyle = "blue";
+Jctx.stroke();
+
+Jctx.moveTo(200,0);// x-axis y-axis
+Jctx.lineTo(400,400);
+Jctx.lineWidth = 10;
+Jctx.strokeStyle = "blue";
+Jctx.stroke();
+
+Jctx.moveTo(100,200);// x-axis y-axis
+Jctx.lineTo(300,200);
+Jctx.lineWidth = 10;
 Jctx.strokeStyle = "blue";
 Jctx.stroke();
