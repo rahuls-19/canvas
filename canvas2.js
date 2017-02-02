b6b04f885a3b087cab4805185a88fcfb54b456eb
Rahul kumar <lit2015023@iiita.ac.in>
2017-02-02 22:45:03 +0530

    canvas creating lines

---
+++ canvas2.js
@@ -0,0 +1,6 @@
+var can = document.getElementById("Newcanvas");
+var Jctx = can.getContext("2d");
+Jctx.moveTo(10,10);// x-axis y-axis
+Jctx.lineTo(200,400);
+Jctx.strokeStyle = "blue";
+Jctx.stroke();
